f78ff7ad1bb0cb414dd2984642a5e26b61dd6c13
Abhijeet Dixit <abhijeetdixit27@gmail.com>
2019-09-15 14:58:18 +0530

    Fixed the line number display and added tabs

--- TextEditor/EditorMain.py
+++ TextEditor/EditorMain.py
@@ -3,6 +3,7 @@ import Tkinter
 import tkMessageBox
 import tkFileDialog
 from MenuFactory import *
+import ttk
 
 class Editor(object):
 	"""docstring for Editor"""
@@ -10,54 +11,72 @@ class Editor(object):
 		self.currentFile = fileName
 		self.rootWindow = Tkinter.Tk()
 		self.rootWindow.tk.eval('package require Tix')
+		self.rootWindow.bind('<Configure>', self.resize)
+		self.rootWindow.bind_all
+
+	def resize(self, event):
+		w,h = self.rootWindow.winfo_width(), self.rootWindow.winfo_height()
+		hb = self.bottomFrame.winfo_height()
+		ht = self.topFrame.winfo_height()
+		self.middleFrame.config(width=w, height=h-hb-ht)
+		self.text.config(width=w, height=h-hb-ht)
+		pass
 
 	def addWidgetsNRun(self):
 		textVar = Tkinter.StringVar()
 		# Adding a mainFrame that contains all other widgets
-		mainFrame = Tkinter.Frame(self.rootWindow)
-		mainFrame.pack()
+		self.mainFrame = Tkinter.Frame(self.rootWindow)
+		self.mainFrame.grid(row=0, column=0)
 
 		# Adding a top and bottom frame for menu and text area
 		# respectively
-		middleFrame = Tkinter.Frame(self.rootWindow)
-		middleFrame.pack()
+		self.middleFrame = Tkinter.Frame(self.rootWindow)
+		self.middleFrame.grid(row=1, column=0)
 
-		topFrame = Tkinter.Frame(self.rootWindow)
-		topFrame.pack(side=Tkinter.TOP)
+		self.topFrame = Tkinter.Frame(self.rootWindow)
+		#self.topFrame.pack(side=Tkinter.TOP)
+		self.topFrame.grid(row=0, column=0)
 
-		bottomFrame = Tkinter.Frame(self.rootWindow, bd=1)
-		bottomFrame.pack(side=Tkinter.BOTTOM)
+		self.bottomFrame = Tkinter.Frame(self.rootWindow, bd=1)
+		#self.bottomFrame.pack(side=Tkinter.BOTTOM)
+		self.bottomFrame.grid(row=2, column=0)
+
+		self.tabControl = ttk.Notebook(self.middleFrame)
+		self.tab1 = ttk.Frame(self.tabControl)
+		self.tabControl.add(self.tab1, text='unsaved')
 
 		# Adding scrollbars
-		VScrollBar = Tkinter.Scrollbar(middleFrame)
-		VScrollBar.pack(side=Tkinter.RIGHT, fill=Tkinter.Y)
+		self.VScrollBar = Tkinter.Scrollbar(self.tab1)
+		self.VScrollBar.pack(side=Tkinter.RIGHT, fill=Tkinter.Y)
 
-		HScrollBar = Tkinter.Scrollbar(middleFrame,orient=Tkinter.HORIZONTAL)
-		HScrollBar.pack(side=Tkinter.BOTTOM, fill=Tkinter.X)
+		self.HScrollBar = Tkinter.Scrollbar(self.tab1,orient=Tkinter.HORIZONTAL)
+		self.HScrollBar.pack(side=Tkinter.BOTTOM, fill=Tkinter.X)
 
 		# Adding a text area where all the text goes
-		text = Tkinter.Text(middleFrame,undo=True,bd=1)
-		text.insert(Tkinter.INSERT,"")
-		text.pack()
+		self.text = Tkinter.Text(self.tab1,undo=True,bd=1)
+		self.text.insert(Tkinter.INSERT,"")
+		self.text.pack()
 
 		# Attaching scrollbars to Text
-		text.config(yscrollcommand=VScrollBar.set)
-		VScrollBar.config(command=text.yview)
-		text.config(xscrollcommand=HScrollBar.set)
-		HScrollBar.config(command=text.xview)
+		self.text.config(yscrollcommand=self.VScrollBar.set)
+		self.VScrollBar.config(command=self.text.yview)
+		self.text.config(xscrollcommand=self.HScrollBar.set)
+		self.HScrollBar.config(command=self.text.xview)
 
-		statusLabel = Tkinter.Label(bottomFrame, bd=1, anchor=Tkinter.E, textvariable=textVar, font=('ariel',12,'normal'))
-		statusLabel.grid(row=0,column=0,sticky=Tkinter.W)
+		self.tabControl.pack(expand=1, fill='both')
+
+		self.statusLabel = Tkinter.Label(self.bottomFrame, bd=1, anchor=Tkinter.E, textvariable=textVar, font=('ariel',12,'normal'))
+		self.statusLabel.grid(row=0,column=0,sticky=Tkinter.W)
 		textVar.set('Ln 0,Col 0')
 
 		# Instantiating a createmenu class and creating menu
 		createmenu = createMenu()
-		menuBar = Tkinter.Menu(topFrame)
-		filemenu = createmenu.createM(0, self.rootWindow, menuBar, text)
-		editmenu = createmenu.createM(1, self.rootWindow, menuBar, text)
-		formatmenu = createmenu.createM(2, self.rootWindow, menuBar, text)
-		viewmenu = createmenu.createM(3, self.rootWindow, menuBar, text)
-		helpmenu = createmenu.createM(4, self.rootWindow, menuBar, text)
+		menuBar = Tkinter.Menu(self.topFrame)
+		filemenu = createmenu.createM(0, self.rootWindow, menuBar, self.text)
+		editmenu = createmenu.createM(1, self.rootWindow, menuBar, self.text)
+		formatmenu = createmenu.createM(2, self.rootWindow, menuBar, self.text)
+		viewmenu = createmenu.createM(3, self.rootWindow, menuBar, self.text)
+		helpmenu = createmenu.createM(4, self.rootWindow, menuBar, self.text)
 		menuBar.add_cascade(label='File',menu=filemenu)
 		menuBar.add_cascade(label='Edit',menu=editmenu)
 		menuBar.add_cascade(label='Format',menu=formatmenu)
